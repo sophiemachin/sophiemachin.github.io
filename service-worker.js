0cce0337efd090b97ecb363711a86c636f340a59
sem <sem@pearson.co.uk>
2019-05-28 11:45:03 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.92b0d793322134925b94dfa4831de6ac.js"
 );
 
 self.addEventListener('message', (event) => {
