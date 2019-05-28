790619721238ab9aaa9cc96bd99ced6694f4b922
sem <sem@pearson.co.uk>
2019-05-28 16:11:49 +0100

    index.js: Router and AppBar

--- src/index.js
+++ src/index.js
@@ -3,10 +3,34 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
+import { Typography as T } from '@material-ui/core'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Users = () => <h1>Users</h1>
+const Contact = () => <h1>Contact</h1>
+const Notfound = () => <h1>Not found</h1>
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+const routing = (
+  <div>
+    <AppBar position="static">
+      <Toolbar>
+          <T variant="body1">Sophie Machin: portfolio</T>
+        </Toolbar>
+      </AppBar>
+
+  <Router>
+    <Switch>
+      <Route path="/" exact component={App} />
+      <Route path="/users" exact component={Users} />
+      <Route path="/contact" exact component={Contact} />
+      <Route component={Notfound} />
+    </Switch>
+  </Router>
+  </div>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
+
 serviceWorker.unregister();
