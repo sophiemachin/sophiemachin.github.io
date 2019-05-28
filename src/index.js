44a41617ea245b96eda31b81e07b0d0c92070b15
sem <sem@pearson.co.uk>
2019-05-28 16:44:30 +0100

    index.js: use AppBarDrawer

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
-import { Typography as T } from '@material-ui/core'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
+import AppBarDrawer from './AppBarDrawer'
+
 
 const Users = () => <h1>Users</h1>
 const Contact = () => <h1>Contact</h1>
@@ -14,11 +13,7 @@ const Notfound = () => <h1>Not found</h1>
 
 const routing = (
   <div>
-    <AppBar position="static">
-      <Toolbar>
-          <T variant="body1">Sophie Machin: portfolio</T>
-        </Toolbar>
-      </AppBar>
+  <AppBarDrawer />
 
   <Router>
     <Switch>
