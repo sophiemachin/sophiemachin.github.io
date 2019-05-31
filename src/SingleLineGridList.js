25425764570415c389ac4967cea6fe7139986a4b
Sophie Machin <sophiemachin@gmail.com>
2019-05-31 08:54:12 +0100

    SingleLineGridList: add cursor pointer to image, remove GridListTileBar, remove modal Paper

--- src/SingleLineGridList.js
+++ src/SingleLineGridList.js
@@ -1,14 +1,8 @@
 import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import { GridList } from '@material-ui/core';
+import {makeStyles} from '@material-ui/core/styles';
+import {GridList} from '@material-ui/core';
 import GridListTile from '@material-ui/core/GridListTile';
-import IconButton from '@material-ui/core/IconButton'
-
-import OpenInBrowser from '@material-ui/icons/OpenInBrowser'
-import GridListTileBar from '@material-ui/core/GridListTileBar'
 import Modal from '@material-ui/core/Modal'
-import Paper from '@material-ui/core/Paper'
-import Button from '@material-ui/core/Button'
 
 const useStyles = makeStyles(theme => ({
     root: {
@@ -31,20 +25,22 @@ const useStyles = makeStyles(theme => ({
         background:
             'linear-gradient(to top, rgba(0,0,0,0.7) 0%, rgba(0,0,0,0.3) 70%, rgba(0,0,0,0) 100%)',
     },
-  modalPaper: {
-      margin: 'auto'
-  },
-  modal: {
-      display: 'flex',
-      alignItems:'center',
-      justifyContent:'center',
-  },
-  modalImage : {
-     width: '100%',
-      height: 'auto',
-      /* Magic! */
-      maxWidth: '50vw',
-  }
+    img: {
+        cursor: 'pointer',
+    },
+    modalPaper: {
+        margin: 'auto'
+      },
+    modal: {
+        display: 'flex',
+        alignItems:'center',
+        justifyContent:'center',
+      },
+    modalImage : {
+        width: '100%',
+        height: 'auto',
+        maxWidth: '50vw',
+      }
 }));
 
 function SingleLineGridList({tileData}) {
@@ -67,32 +63,13 @@ function SingleLineGridList({tileData}) {
     return (
         <div className={classes.root}>
           <Modal open={open} onClose={handleModalClose} className={classes.modal}>
-            <Paper className={classes.modalPaper}>
-              <div>
                 <img src={image} alt='Modal image' className={classes.modalImage}/>
-                <Button onClick={handleModalClose}>Close</Button>
-              </div>
-            </Paper>
           </Modal>
             <GridList className={classes.gridList} cols={2.5}>
                 {tileData.map(tile => (
                     <GridListTile key={tile.img} className={classes.tile}>
-                        <img src={tile.img} alt={tile.title} />
-
-                        <GridListTileBar
-                          title={tile.title}
-                          classes={{
-                            root: classes.titleBar,
-                            title: classes.title,
-                          }}
-                          actionIcon={
-                            <IconButton onClick={() => handleModalOpen(tile.img)}>
-                              <OpenInBrowser
-                                className={classes.title}
-                              />
-                            </IconButton>
-                          }
-                        />
+                        <img src={tile.img} alt={tile.title} className={classes.img}
+                             onClick={() => handleModalOpen(tile.img)} />
                     </GridListTile>
                 ))}
             </GridList>
