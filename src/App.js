9f3ddc748e1346d6466a9bb8c284fc2ec89f09a1
Sophie Machin <sophiemachin@gmail.com>
2019-06-01 20:59:56 +0100

    App.js: add icons to Cards, remove images

--- src/App.js
+++ src/App.js
@@ -3,14 +3,65 @@ import './App.css';
 
 import {makeStyles} from '@material-ui/core/styles';
 
-import burgers from './images/burgers.jpg'
-import react from './images/react.png'
-import plotly from './images/plotly.png'
-import matlab from './images/matlab.jpg'
-
-import {Card, CardActionArea, CardHeader, CardMedia, Grid} from '@material-ui/core'
+import {Card, CardActionArea, CardHeader, Grid} from '@material-ui/core'
 
 import SimpleBreadCrumbs from "./BreadCrumbs";
+import Avatar from "@material-ui/core/Avatar";
+
+const tileData = [
+  {
+    title: 'Data visualisation',
+    language: 'React, Plotly, Django',
+    cols: 2,
+    href: '/#/data',
+    icons : [
+      'fas fa-database',
+      'fab fa-python',
+      'fas fa-chart-bar',
+    ],
+  }, {
+    title: 'Image Processing',
+    language: 'MATLAB and Octave',
+    cols: 2,
+    href: '/#/images',
+    icons: [
+      'fas fa-image',
+      'fas fa-sliders-h',
+
+    ],
+  }, {
+    title: 'Front End',
+    language: 'React',
+    cols: 2,
+    href: '/#/frontend',
+    icons : [
+      'devicon-webpack-plain',
+      'fab fa-js',
+      'fab fa-react',
+    ],
+  }, {
+    title: 'Puzzles',
+    language: 'C++ and Python',
+    cols: 2,
+    href: '/#/puzzles',
+    icons: [
+      'fas fa-puzzle-piece',
+      'devicon-cplusplus-plain',
+    ],
+  }, {
+    title: 'Workflow',
+    language: 'Workflow tools',
+    cols: 2,
+    href: '/#/workflow',
+    icons: [
+      'devicon-github-plain',
+      'devicon-pycharm-plain',
+      'devicon-trello-plain',
+  ],
+},
+
+];
+
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -28,35 +79,11 @@ const useStyles = makeStyles(theme => ({
     height: 0,
     paddingTop: '56.25%', // 16:9
   },
+  avatar:{
+    margin : theme.spacing(0.5),
+  },
 }))
 
-const tileData = [
-  {
-    img: plotly,
-    title: 'Data visualisation',
-    language: 'React, Plotly, Django',
-    cols: 2,
-    href: '/#/data',
-  }, {
-    img: matlab,
-    title: 'Image Processing',
-    language: 'MATLAB and Octave',
-    cols: 2,
-    href: '/#/images',
-  }, {
-    img: react,
-    title: 'Front End',
-    language: 'React',
-    cols: 2,
-    href: '/#/frontend',
-  }, {
-    img: burgers,
-    title: 'Puzzles',
-    language: 'C++ and Python',
-    cols: 2,
-    href: '/#/puzzles',
-  }
-];
 
 function App() {
 
@@ -72,10 +99,14 @@ function App() {
             <Card className={classes.card}>
             <CardActionArea href={tile.href}>
               <CardHeader title={tile.title} subheader={tile.language}/>
-              <CardMedia
-                className={classes.media}
-                image={tile.img}
-              />
+              <Grid container justify="center" alignItems="center">
+              {tile.icons.map(icon =>
+                <Avatar key={icon} className={classes.avatar}>
+                  <i key={icon} className={`${icon}`}/>
+                </Avatar>
+              )}
+
+              </Grid>
               </CardActionArea>
 
             </Card>
