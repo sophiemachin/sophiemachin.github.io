7d95a0d241c340233002d71ef95f4141e878dd02
Sophie Machin <sophiemachin@gmail.com>
2019-09-18 15:56:16 +0100

    Contact: add success message

--- src/Contact.js
+++ src/Contact.js
@@ -18,6 +18,7 @@ const useStyles = makeStyles(theme => ({
   card: {
     margin: theme.spacing(1),
     // textAlign: 'center',
+    maxWidth: '600px',
   },
   avatar:{
     margin : theme.spacing(0.5),
@@ -30,7 +31,21 @@ const useStyles = makeStyles(theme => ({
   textField : {
     minWidth:'400px',
     margin: '5px',
+  },
+  success : {
+    padding: '20px',
+    minHeight: '300px',
+  },
+  messageReadout: {
+    color: 'grey',
+    fontStyle: 'italic',
+  },
+  successIntro: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
   }
+
 
 }));
 
@@ -41,13 +56,14 @@ function Contact() {
   const [feedback, setFeedback] = React.useState('');
   const [name, setName] = React.useState('');
   const [email, setEmail] = React.useState('');
-
+  const [success, setSuccess] = React.useState(false);
 
   function sendFeedback (templateId, variables) {
     window.emailjs.send(
       'gmail', templateId,
       variables
     ).then(res => {
+      setSuccess(true)
       console.log('Email successfully sent!')
     })
     // Handle errors here however you like, or use a React error boundary
@@ -63,52 +79,71 @@ function Contact() {
     })
   }
 
+  let content = <div className={classes.success}>
+    <div className={classes.successIntro}>
+    <i className="fas fa-check" style={{paddingRight:'15px'}}/>
+    <T gutterBottom>
+      Your message was sent successfully
+    </T>
+    </div>
+      <div className={classes.messageReadout}>
+      <T gutterBottom>From: {name}</T>
+      <T gutterBottom>Email: {email}</T>
+      <T gutterBottom>Message: {feedback}</T>
+    </div>
+  </div>
+  if (!success) {
+    content = <form className="test-mailing">
+      <div className={classes.form}>
+        <TextField
+          className={classes.textField}
+          variant="outlined"
+          label='Name'
+          onChange={e => setName(e.target.value)}
+          value={name}
+        />
+        <TextField
+          className={classes.textField}
+          variant="outlined"
+          label='Email address'
+          onChange={e => setEmail(e.target.value)}
+          value={email}
+        />
+        <TextField
+          className={classes.textField}
+          variant="outlined"
+          onChange={e => setFeedback(e.target.value)}
+          value={feedback}
+          multiline
+          rows="4"
+          label="Message"
+          required
+        />
+      </div>
+      <Button
+        variant='contained'
+        color='primary'
+        onClick={handleSubmit}
+        disabled={!feedback}
+      >
+        Submit
+      </Button>
+    </form>
+  }
+
   return <div className={classes.root}>
     <div className={classes.content}>
       <Card className={classes.card}>
         <CardHeader title='Get in touch!' />
         <CardContent >
           <T variant='body1'>If you're interested in a collaboration or want
-            to use one of my projects, I'd love to hear from you. </T>
+            to use one of my projects, I'd love to hear from you. If you're
+            expecting a reply please leave your email address.
+          </T>
 
         <Grid container justify="center" alignItems="center">
+          {content}
 
-          <form className="test-mailing">
-            <div className={classes.form}>
-              <TextField
-                className={classes.textField}
-                variant="outlined"
-                label='Name'
-                onChange={e => setName(e.target.value)}
-                value={name}
-              />
-              <TextField
-                className={classes.textField}
-                variant="outlined"
-                label='Email address'
-                onChange={e => setEmail(e.target.value)}
-                value={email}
-              />
-              <TextField
-                className={classes.textField}
-                variant="outlined"
-                onChange={e => setFeedback(e.target.value)}
-                value={feedback}
-                multiline
-                rows="4"
-                label="Message"
-                required
-              />
-            </div>
-            <Button
-              variant='contained'
-              color='primary'
-              onClick={handleSubmit}
-              disabled={!feedback}
-            >
-              Submit
-            </Button>
-          </form>
         </Grid>
         </CardContent>
       </Card>
